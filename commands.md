3312270d3b6bebacd96833d73c0ef41c1f48550d
eduperezsoto <eduardop141@hotmail.com>
2025-05-27 20:36:27 +0200

    Actions updated and sonarqube configured

--- commands.md
+++
@@ -1,2 +0,0 @@
-docker-compose up -d web
-docker-compose run --rm tests
--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name   = "rg-terraform-backend"
-    storage_account_name  = "stterraformstate"
+    storage_account_name  = "saterraformstatetfm"
     container_name        = "tfstate"
     key                   = "azureops-platform.terraform.tfstate"
   }
